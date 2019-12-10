6e6f20380d1efe56c72b6ee09163236d6e8eadb6
Travis Hammond <dashdeckers@gmail.com>
2019-12-10 02:54:35 +0100

    MyPy conformant

--- Digits/digits.py
+++ Digits/digits.py
@@ -1,6 +1,8 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
-import time
+
 
 def show_digit(digit, col_vector=False):
     ''' Show the digit as an image.
@@ -25,12 +27,13 @@ def show_digit(digit, col_vector=False):
     plt.imshow(digit, cmap='Greys')
     plt.show()
 
+
 def try_it_out(W, Um, digit):
     ''' Visually / Manually try out the classifier.
 
-    W is the classifier itself that can make a prediction given a feature vector
-    Um is the feature 'mapper' that can reduce the dimensionality of raw input
-    digit is the raw input (as a column vector) of a digit
+    W is the classifier itself that can make a prediction given a feature
+    vector Um is the feature 'mapper' that can reduce the dimensionality of raw
+    input digit is the raw input (as a column vector) of a digit
 
     Usage:
         ...
@@ -52,6 +55,7 @@ def try_it_out(W, Um, digit):
     print(f'Best guess: {np.argmax(pred)}')
     show_digit(digit, col_vector=True)
 
+
 def load_data(filename='mfeat-pix.txt'):
     ''' Loads the 'Project Digits' dataset from file and splits it into
     test and train as per the project instructions. Also creates the array
@@ -64,24 +68,25 @@ def load_data(filename='mfeat-pix.txt'):
     '''
     with open(filename, 'r') as datafile:
         # Load the transposed datafile to get each image in a col vector
-        data = np.loadtxt(datafile).T # (240, 2000)
+        data = np.loadtxt(datafile).T  # (240, 2000)
 
         # Create an array of labels (each 200 elements is a digit)
         labels = np.zeros(data.shape[1], dtype=np.int)
         for digit in range(10):
-            labels[digit * 200: (digit+1) * 200] = digit # (2000,)
+            labels[digit * 200: (digit+1) * 200] = digit  # (2000,)
 
         # Split the data into train and test by first determining the indices
-        even = np.array([np.arange(i*100, (i+1)*100) for i in range(0,20,2)])
-        odd  = np.array([np.arange(i*100, (i+1)*100) for i in range(1,20,2)])
-        even = even.reshape(-1) # reshape to get a single,
-        odd  = odd.reshape(-1)  # long array of indices: (1000, )
+        even = np.array([np.arange(i*100, (i+1)*100) for i in range(0, 20, 2)])
+        odd = np.array([np.arange(i*100, (i+1)*100) for i in range(1, 20, 2)])
+        even = even.reshape(-1)  # reshape to get a single,
+        odd = odd.reshape(-1)    # long array of indices: (1000, )
 
         # And then selecting via the array of indices
         x_train, y_train = data[:, even], labels[even]
-        x_test,  y_test  = data[:, odd],  labels[odd]
+        x_test,  y_test = data[:, odd],  labels[odd]
 
         return (x_train, y_train), (x_test, y_test)
+
 
 def preprocess_data(data, subtract_mean=False):
     ''' Preprocess the data from load_data by normalizing the values to the
@@ -103,12 +108,13 @@ def preprocess_data(data, subtract_mean=False):
 
     # Center the data by subtracting the mean
     if subtract_mean:
-        mean = x_train.mean(axis=1).reshape(-1, 1) # (240, 1) (=column vector)
+        mean = x_train.mean(axis=1).reshape(-1, 1)  # (240, 1) (=column vector)
         assert mean.shape == (240, 1)
         x_train -= mean
         x_test -= mean
 
     return (x_train, y_train), (x_test, y_test)
+
 
 def compute_first_m_PCs_of_x(x, m):
     ''' Compute the first m principle components of the dataset matrix x.
@@ -122,6 +128,7 @@ def compute_first_m_PCs_of_x(x, m):
     U, S, V = np.linalg.svd(C)
     # Return the first m columns of U
     return U[:, :m]
+
 
 def one_hot_encode_labels(labels):
     ''' Returns a matrix of one-hot encoded vectors for the given array
@@ -141,6 +148,7 @@ def one_hot_encode_labels(labels):
 
     return label_matrix
 
+
 def compute_LR_classifier(F, V):
     ''' Compute the Linear Regression classifier according to:
 
@@ -155,6 +163,7 @@ def compute_LR_classifier(F, V):
 
     return W_transp.T                   # W'
 
+
 def compute_MSE(V, F, W):
     ''' Compute the Mean Squared Error.
 
@@ -164,9 +173,10 @@ def compute_MSE(V, F, W):
 
     '''
     n_examples = V.shape[1]
-    squared_error = np.square(V - np.dot(W, F)) # (10, 1000)
-    mean_squared_error = squared_error.mean(axis=0) # (1000, )
-    return mean_squared_error.sum() / n_examples # (float)
+    squared_error = np.square(V - np.dot(W, F))  # (10, 1000)
+    mean_squared_error = squared_error.mean(axis=0)  # (1000, )
+    return mean_squared_error.sum() / n_examples  # (float)
+
 
 def compute_MR(V, F, W):
     ''' Compute the Misclassification Rate.
@@ -183,10 +193,11 @@ def compute_MR(V, F, W):
     '''
     n_examples = V.shape[1]
 
-    pred = np.dot(W, F) # (10, n_examples)
-    best = np.argmax(pred, axis=0) # (n_examples, )
-    true = np.argmax(V, axis=0) # (n_examples, )
-    return (n_examples - np.count_nonzero(true==best)) / n_examples
+    pred = np.dot(W, F)  # (10, n_examples)
+    best = np.argmax(pred, axis=0)  # (n_examples, )
+    true = np.argmax(V, axis=0)  # (n_examples, )
+    return (n_examples - np.count_nonzero(true == best)) / n_examples
+
 
 if __name__ == '__main__':
     t0 = time.time()
@@ -196,9 +207,9 @@ if __name__ == '__main__':
     print(f'Loaded and preprocessed data ({time.time() - t0})')
 
     MSE_train_vals = list()
-    MSE_test_vals  = list()
-    MR_train_vals  = list()
-    MR_test_vals   = list()
+    MSE_test_vals = list()
+    MR_train_vals = list()
+    MR_test_vals = list()
     m_vals = list(range(241))
 
     for m in m_vals:
@@ -207,12 +218,12 @@ if __name__ == '__main__':
         # Step 1: PCA
         Um = compute_first_m_PCs_of_x(x_train, m)
         F_train = np.dot(Um.T, x_train)
-        F_test  = np.dot(Um.T, x_test)
+        F_test = np.dot(Um.T, x_test)
         print(f'Computed PCA feature vectors ({time.time() - t0})')
 
         # Step 2: One-Hot Encode Labels
         V_train = one_hot_encode_labels(y_train)
-        V_test  = one_hot_encode_labels(y_test)
+        V_test = one_hot_encode_labels(y_test)
         print(f'One-Hot encoded labels ({time.time() - t0})')
 
         # Step 3: Compute LR Classifier
