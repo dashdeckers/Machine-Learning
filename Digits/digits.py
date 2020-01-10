147acb7705c393780eb8703772027cc239a8351a
Travis Hammond <dashdeckers@gmail.com>
2020-01-10 15:37:51 +0100

    Pretty much reworked most of it to use sklearn, added a do_everything function to show how we could just replace most of the old code in the main. Still needs an add_noise function and a small rework of the load_data function (which should be combined with label_data)

--- Digits/digits.py
+++ Digits/digits.py
@@ -1,13 +1,20 @@
 """A Linear Regression baseline algorithm for predicting handwritten digits."""
 
+import pathlib
 import sys
 import time
-import pathlib
+from functools import partial
+
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
+from sklearn.decomposition import PCA
+from sklearn.linear_model import RidgeClassifier
+from sklearn.metrics import (accuracy_score, classification_report,
+                             confusion_matrix, precision_recall_fscore_support)
+from sklearn.model_selection import ParameterGrid, RepeatedStratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 
 def timestamp(start_time):
@@ -39,38 +46,9 @@ def show_digit(digit, col_vector=False):
     plt.show()
 
 
-def try_it_out(W, Um, digit):
-    """Visually / Manually try out the classifier.
-
-    W is the classifier itself that can make a prediction given a feature
-    vector Um is the feature 'mapper' that can reduce the dimensionality of raw
-    input digit is the raw input (as a column vector) of a digit
-
-    Usage:
-        ...
-        # Keep a decent classifier + feature mapping to try out later
-        if m == 30:
-            W_keep = W
-            Um_keep = Um
-        ...
-
-        digit = x_test[:, arbitrary_index]
-        try_it_out(W_keep, Um_keep, digit)
-
-    """
-    # Reduce dimensionality of the digit (=get feature vector for digit)
-    f_digit = np.dot(Um.T, digit)
-    # Make prediction on the feature vector (=get probability vector)
-    pred = np.dot(W, f_digit)
-    # Print best guess and show the digit
-    print(f'Best guess: {np.argmax(pred)}')
-    show_digit(digit, col_vector=True)
-
-
 def label_data(datafile):
     # Load the transposed datafile to get each image in a col vector
     data = np.loadtxt(datafile).T  # (240, 100c)
-    print(data.shape)
 
     # Label every 10% as a new digit
     digitreps = int(data.shape[1] / 10)
@@ -100,245 +78,192 @@ def load_data(s=0.0, c=1):
     testpath = pathlib.Path(__file__).parent / 'testdata/testdata.txt'
     trainpath = pathlib.Path(__file__).parent / \
         ('traindata/traindata_s_' + str(s) + '_c_' + str(c) + '.txt')
-    testfile = open(testpath, 'r')
-    trainfile = open(trainpath, 'r')
 
-    x_train, y_train = label_data(trainfile)
-    x_test, y_test = label_data(testfile)
-
-    print(len(x_train))
-    print(len(y_train))
-    print(len(x_test))
-    print(len(y_test))
+    x_train, y_train = label_data(trainpath)
+    x_test, y_test = label_data(testpath)
 
     return (x_train, y_train), (x_test, y_test)
 
 
-def preprocess_data(data, subtract_mean=False):
-    """Preprocess the data.
+def results(model, data, labels, show_list=[]):
+    """Show and/or return the results.
 
-    Preprocess the data from load_data by normalizing the values to the
-    range [0,1] and centering the data by subtracting the mean.
+    Print metrics by passing the names of the metrics via show_list like so:
+    show_list=['prec', 'rec', 'F1', 'acc', 'MR', 'matrix', 'report']
 
-    Centering the data actually decreases performance.. lowest MSE_train ~ -0.5
-
-    """
-    (x_train, y_train), (x_test, y_test) = data
-
-    # Normalize the data to range [0,1] (assuming range [0,6])
-    x_train /= 6
-    x_test /= 6
-
-    assert np.max(x_train) <= 1, np.max(x_train)
-    assert np.max(x_test) <= 1, np.max(x_test)
-    assert np.min(x_train) >= 0, np.min(x_train)
-    assert np.min(x_test) >= 0, np.min(x_test)
-
-    # Center the data by subtracting the mean
-    if subtract_mean:
-        mean = x_train.mean(axis=1).reshape(-1, 1)  # (240, 1) (=column vector)
-        assert mean.shape == (240, 1)
-        x_train -= mean
-        x_test -= mean
-
-    return (x_train, y_train), (x_test, y_test)
-
-
-def compute_first_m_PCs_of_x(x, m):
-    """Compute the first m principle components of the data-matrix x.
-
-    Um.shape == (240, m)
+    Always returns the following metrics:
+    ['prec', 'rec', 'F1', 'acc', 'MR']
 
     """
-    # Compute covariance matrix
-    C = np.cov(x)
-    # Get the SVD of C
-    U, S, V = np.linalg.svd(C)
-    # Return the first m columns of U
-    return U[:, :m]
+    # Don't allow invalid metrics in show_list
+    valid_metrics = ['prec', 'rec', 'F1', 'acc', 'MR', 'matrix', 'report']
+    assert all(metric in valid_metrics for metric in show_list)
+
+    # Gather the data
+    pred = model.predict(data)  # This is the line that takes so long...
+    precs, recs, F1s, _ = precision_recall_fscore_support(labels, pred)
+    acc = accuracy_score(labels, pred)
+
+    if show_list:
+        # Helper function to print the mean and standard deviation of a list
+        def show_mean_std(n, l):
+            print(f'{n.title()}: mean={np.mean(l):.3f}, std={np.std(l):.3f}')
+
+        # Possible metrics as partial functions
+        show_dict = {
+            'prec': partial(show_mean_std, 'precision', precs),
+            'rec': partial(show_mean_std, 'recall', recs),
+            'F1': partial(show_mean_std, 'f1-score', F1s),
+            'acc': partial(print, f'Accuracy: {acc:.3f}'),
+            'MR': partial(print, f'Misclassification Rate: {1-acc:.3f}'),
+            'matrix': partial(confusion_matrix, labels, pred),
+            'report': partial(classification_report, labels, pred)
+        }
+
+        for metric in show_list:
+            show_dict[metric]()
+
+    return {
+        'prec': [np.mean(precs), np.std(precs)],
+        'rec': [np.mean(recs), np.std(recs)],
+        'F1': [np.mean(F1s), np.std(F1s)],
+        'acc': acc,
+        'MR': 1-acc,
+    }
 
 
-def one_hot_encode_labels(labels):
-    """Return a One-Hot encoded representation of digit labels.
+def cross_val(pipeline, data, labels, n_splits=10, n_repeats=1):
+    """Perform cross-validation while adding noise."""
+    cv = RepeatedStratifiedKFold(
+        n_splits=n_splits,
+        n_repeats=n_repeats,
+        random_state=1,
+    )
 
-    Returns a matrix of one-hot encoded vectors for the given array
-    of values. Assumes that the labels in y correspond to the indices in
-    [0, 9] that should be set to 1 in the resulting matrix.
+    # Let the CV split the data correctly, the correct number of times
+    perf = list()
+    for train_indices, test_indices in cv.split(data, labels):
+        x_train, x_test = data[train_indices], data[test_indices]
+        y_train, y_test = labels[train_indices], labels[test_indices]
 
-        label_matrix.shape == (10, n_labels)
+        # Add noise + labels to x_train + y_train here, like:
+        # x_train, y_train = add_noise(x_train, y_train)
 
+        # Fit the model and collect the results (its performance)
+        pipeline.fit(x_train, y_train)
+        perf.append(results(pipeline, x_test, y_test))
+
+    return perf
+
+
+def param_sweep_LR(pipeline, data, labels, m_vals=[33], alphas=[0]):
+    """Perform a parameter sweep on the Linear Regression pipeline.
+
+    Pass in the full dataset and labels, and specify which parameters
+    to sweep for as lists. To use this on a KNN pipeline, simply change
+    the keyword arguments in set_params() accordingly.
     """
-    assert labels.ndim == 1, (labels, labels.shape)
+    # Generate all possible combinations, producing a list of dicts
+    params = list(ParameterGrid({
+        'm': m_vals,
+        'alpha': alphas,
+    }))
 
-    N = len(labels)
-    # Create a matrix of zeros
-    label_matrix = np.zeros(shape=(10, N))
-    # Entries with (row, col) == (label, c_index) should equal 1
-    label_matrix[labels, range(N)] = 1
+    # Perform cross validation for each parameter combination
+    results = list()
+    for param_set in params:
+        pipeline.set_params(pca__n_components=param_set['m'])
+        pipeline.set_params(LR__alpha=param_set['alpha'])
 
-    return label_matrix
+        perf = cross_val(pipeline, data, labels)
+        results.append((param_set, perf))
+
+    return results
 
 
-def compute_LR_classifier(F, V, alpha=0):
-    """Compute the Linear Regression classifier according to lecture notes.
+def do_everything():
+    """Perform the full experiment.
 
-    It includes the Ridge Regression regularization term (eq. 47).
-
-    W' = (F F' + a^2 * I)^-1 F V'
-
+    This is just to show how the old code in the main could
+    be replaced.
     """
-    Reg_Term = alpha**2 * np.identity(F.shape[0])
+    # Load the data, make the labels
+    data = np.loadtxt('mfeat-pix.txt')
+    labels = np.zeros(data.shape[0], dtype=np.int)
+    digitreps = int(data.shape[0] / 10)
+    for digit in range(10):
+        labels[digit * digitreps: (digit+1) * digitreps] = digit
 
-    F_Fprime = np.dot(F, F.T)           # F*F'
-    RidgeAdd = F_Fprime + Reg_Term      # (F*F' + a2*I)
-    F_Fp_inv = np.linalg.inv(RidgeAdd)  # (F*F' + a2*I)^-1
+    # Define the pipeline
+    pipeline = Pipeline([
+        ('normalize', StandardScaler()),
+        ('pca', PCA()),
+        ('LR', RidgeClassifier()),
+    ])
 
-    Triple_F = np.dot(F_Fp_inv, F)      # (F*F' + a2*I)^-1 * F
-    W_transp = np.dot(Triple_F, V.T)    # (F*F' + a2*I)^-1 * F * V'
+    # Do paramsweep with cross validation
+    results = param_sweep_LR(pipeline, data, labels)
 
-    return W_transp.T                   # W'
+    return results
 
-
-def compute_MSE(V, F, W):
-    """Compute the Mean Squared Error.
-
-    Find the difference between predictions (=W*F) and label-vectors, square
-    the result to avoid negative values, then find the mean squared error for
-    each example. Finally, sum these means and divide by n_examples.
-
-    """
-    n_examples = V.shape[1]
-    squared_error = np.square(V - np.dot(W, F))  # (10, 1000)
-    mean_squared_error = squared_error.mean(axis=0)  # (1000, )
-    return mean_squared_error.sum() / n_examples  # (float)
-
-
-def compute_MR(V, F, W):
-    """Compute the Misclassification Rate.
-
-    Calculate all prediction vectors, then find the argmax of each vector to
-    get an array containing the best guesses for each example in F.
-
-    Similarly, find the argmax of each one-hot encoded label vector in V to get
-    an array containing all the true answers for each example in F.
-
-    Then, count the number of times the answer was incorrect and divide by
-    n_examples.
-
-    """
-    n_examples = V.shape[1]
-
-    pred = np.dot(W, F)  # (10, n_examples)
-    best = np.argmax(pred, axis=0)  # (n_examples, )
-    true = np.argmax(V, axis=0)  # (n_examples, )
-    return (n_examples - np.count_nonzero(true == best)) / n_examples
-
-
-def show_results(y_test, pred, verbose=True):
-    """Show all the results.
-
-    First print the confusion matrix, and then the precision and recall values.
-
-    """
-    precs, recs, *_ = precision_recall_fscore_support(y_test, pred)
-    n_examples = y_test.shape[0]
-    MR = (n_examples - np.count_nonzero(y_test == pred)) / n_examples
-
-    if verbose:
-        print(confusion_matrix(y_test, pred))
-
-        print('')
-        print('Class - Precision - Recall')
-        for digit, prec, rec in zip(range(10), precs, recs):
-            print(f' {str(digit):4s} - {str(round(prec, 3)):9s} - {rec}')
-
-        print('')
-        print(f'Precision (mean, std): {np.mean(precs):.3f}, {np.std(precs):.3f}')  # noqa
-        print(f'Recall (mean, std): {np.mean(recs):.3f}, {np.std(recs):.3f}')
-
-        print('')
-        print(f'Misclassification Rate: {MR:.3f}')
-
-    return (MR, precs, recs)
-
-
-def get_LR_pred(W, F_test):
-    """Get the predictions from the weights of the Linear Regression model."""
-    pred = np.dot(W, F_test)
-    return np.argmax(pred, axis=0)
-
-
-def get_KNN_pred(x_train, y_train, x_test):
-    """Fit a KNN model on the data and return the predictions."""
-    y_train = LabelEncoder().fit_transform(y_train)
-    KNN_model = KNeighborsClassifier().fit(x_train.T, y_train)
-    return KNN_model.predict(x_test.T)
+#########################################################################
 
 
 if __name__ == '__main__':
-
     t0 = time.time()
 
-    # Step 0: Load and preprocess Data
-    if(len(sys.argv) <= 1):
-        (x_train, y_train), (x_test, y_test) = preprocess_data(load_data())
+    # Load data
+    if len(sys.argv) <= 1:
+        (x_train, y_train), (x_test, y_test) = load_data()
     else:
-        (x_train, y_train), (x_test, y_test) = \
-            preprocess_data(load_data(sys.argv[1], sys.argv[2]))
-    print(f'Loaded and preprocessed data ({timestamp(t0)})')
+        (s, c) = (sys.argv[1], sys.argv[2])
+        (x_train, y_train), (x_test, y_test) = load_data(s, c)
 
-    MSE_trains = list()
-    MSE_tests = list()
-    MR_trains = list()
-    MR_tests = list()
-    m_vals = [30]  # list(range(241))
+    # Initialize data structures to capture all necessary info
+    LR_train = list()
+    LR_test = list()
+    KNN_train = list()
+    KNN_test = list()
 
+    # Parameter sweep loop
     alpha = 0
+    m_vals = list(range(1, 241))
     for m in m_vals:
-        print(f'Setting m={m}:')
+        print(f'\n\nSetting m={m}:')
 
-        # Step 1: PCA
-        Um = compute_first_m_PCs_of_x(x_train, m)
-        F_train = np.dot(Um.T, x_train)
-        F_test = np.dot(Um.T, x_test)
-        print(f'Computed PCA feature vectors ({timestamp(t0)})')
+        # Preprocess the data
+        preprocessing = Pipeline([
+            ('normalize', StandardScaler()),
+            ('pca', PCA(n_components=m)),
+        ])
 
-        # Step 2: One-Hot Encode Labels
-        V_train = one_hot_encode_labels(y_train)
-        V_test = one_hot_encode_labels(y_test)
-        print(f'One-Hot encoded labels ({timestamp(t0)})')
+        x_train_processed = preprocessing.fit_transform(x_train.T)
+        x_test_processed = preprocessing.transform(x_test.T)
+        print(f'Preprocessed the data ({timestamp(t0)})')
 
-        # Step 3: Compute LR Classifier
-        W = compute_LR_classifier(F_train, V_train, alpha)
-        print(f'Computed linear regression weight matrix ({timestamp(t0)})')
+        # Fit the models (Only LR runs in 20s, both run in 170s)
+        LR_model = RidgeClassifier().fit(x_train_processed, y_train)
+        KNN_model = KNeighborsClassifier().fit(x_train_processed, y_train)
 
-        # Step 4: Compute the Errors
-        MSE_trains.append(np.log10(compute_MSE(V_train, F_train, W)))
-        MSE_tests.append(np.log10(compute_MSE(V_test, F_test, W)))
-        MR_trains.append(np.log10(compute_MR(V_train, F_train, W)))
-        MR_tests.append(np.log10(compute_MR(V_test, F_test, W)))
+        # Evaluate the models
+        LR_train.append(results(LR_model, x_train_processed, y_train))
+        LR_test.append(results(LR_model, x_test_processed, y_test))
+        KNN_train.append(results(KNN_model, x_train_processed, y_train))
+        KNN_test.append(results(KNN_model, x_test_processed, y_test))
+        print(f'Crunched the numbers ({timestamp(t0)})')
 
-        print(f'Computed the Errors ({timestamp(t0)})')
-        print(f'\tMSE_train error (for m={m}): {MSE_trains[-1]}')
-        print(f'\tMSE_test  error (for m={m}): {MSE_tests[-1]}\n')
+    # Plot the results
+    MR_LR_train = [np.log10(res['MR']) for res in LR_train]
+    MR_LR_test = [np.log10(res['MR']) for res in LR_test]
+    MR_KNN_train = [np.log10(res['MR']) for res in KNN_train]
+    MR_KNN_test = [np.log10(res['MR']) for res in KNN_test]
 
-    # If only one m_val has been chosen, then just compare the results and exit
-    if len(m_vals) == 1:
-        print('\nResults for the Linear Regression model:\n')
-        show_results(y_test, get_LR_pred(W, F_test))
-        print('\nResults for the KNN model:\n')
-        show_results(y_test, get_KNN_pred(x_train, y_train, x_test))
-        print('')
-        sys.exit()
-
-    # Step 5: Plot the results
-
-    plt.plot(m_vals, MSE_trains, c='blue', linestyle='--', label='MSE_train')
-    plt.plot(m_vals, MSE_tests, c='red', linestyle='--', label='MSE_test')
-    plt.plot(m_vals, MR_trains, c='blue', linestyle='-', label='MR_train')
-    plt.plot(m_vals, MR_tests, c='red', linestyle='-', label='MR_test')
+    plt.plot(m_vals, MR_LR_train, c='blue', linestyle='-', label='LR_train')
+    plt.plot(m_vals, MR_LR_test, c='red', linestyle='-', label='LR_test')
+    plt.plot(m_vals, MR_KNN_train, c='blue', linestyle='--', label='KNN_train')
+    plt.plot(m_vals, MR_KNN_test, c='red', linestyle='--', label='KNN_test')
     plt.xlabel('m')
-    plt.ylabel('MSE/MR (log10)')
-    plt.title(f'MSE/MR vs chosen m (with alpha={alpha})')
+    plt.ylabel('MR (log10)')
+    plt.title(f'MR vs chosen m (with alpha={alpha})')
     plt.legend()
     plt.show()
