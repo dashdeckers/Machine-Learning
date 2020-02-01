ac6fb7996d74daa18d7c0ccf44ba1692c1c4c955
Travis Hammond <dashdeckers@gmail.com>
2020-02-01 13:24:24 +0100

    Cleaned up plotter.py

--- Digits/plotter.py
+++ Digits/plotter.py
@@ -1,35 +1,46 @@
 import pickle as pkl
-import numpy as np
+
 import matplotlib.pyplot as plt
-import matplotlib
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import normalize
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D  # noqa
 
 if __name__ == '__main__':
-    with open('resultsLR', 'rb') as f:
+    with open('LR_fullsweep', 'rb') as f:
         data = pkl.load(f)
 
-    results_set = np.zeros((len(data), 3))
-    MR_means = []
-    for i, datapoint in enumerate(data):
-        mean = 0
-        for i in range(0, len(datapoint[1])):
-            mean += datapoint[1][i]['MR']
-        mean /= len(datapoint[1])
-        MR_means.append(mean)
-        results_set[i] = [datapoint[0]['noise_spread'], datapoint[0]['m'], mean]
+    """
+    data = [datapoint]
+    datapoint = (params, results)
+    params = {'param': value}
+    results = [single_result]
+    single_result = {'measure': value}
+    """
 
-    x = [datapoint[0]['noise_spread'] for datapoint in data]
-    y = [datapoint[0]['m'] for datapoint in data]
-    z = MR_means
+    # Set the performance measure to plot.
+    # Can be one of: ['prec', 'rec', 'F1', 'acc', 'MR']
+    measure = 'MR'
 
-    #plt.scatter(x, y, c=z)
+    # Aggregate the performance results together:
+    # Each datapoint has a list of results (k results from k-fold cross-val)
+    if measure in ['F1', 'prec', 'rec']:
+        # These results come in pairs of (mean, std), we only want the mean
+        res_list = [[res[measure][0] for res in dp[1]] for dp in data]
+    else:
+        # The other results are simple values
+        res_list = [[res[measure] for res in dp[1]] for dp in data]
 
+    # Plot the noise spread and the PCA-m on the x- and y- axes
+    x = [dp[0]['noise_spread'] for dp in data]
+    y = [dp[0]['m'] for dp in data]
+
+    # Take the mean of each list of results to plot on the z-axis
+    z = [sum(results) / len(results) for results in res_list]
+
+    # Plot the results
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x,y,z, c=np.negative(z))
+    ax.scatter(x, y, z, c=np.negative(z))
     ax.set_xlabel('Noise spread')
     ax.set_ylabel('Number of Principle Components')
-    ax.set_zlabel('Misclassification rate')
-
-plt.show()
+    ax.set_zlabel(f'Performance ({measure})')
+    plt.show()
