b4d1fbf5582b096a8d8647ab5c45a9d03ff2c367
ivopascal <ivopascal@gmail.com>
2020-02-02 12:53:12 +0100

    Support for surface fitting

--- Digits/plotter.py
+++ Digits/plotter.py
@@ -1,8 +1,41 @@
 import pickle as pkl
 
+import numpy as np
+import scipy.linalg
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa
+
+
+def fit_plane(x, y, z, order=2):
+    data = np.c_[x, y, z]
+    # regular grid covering the domain of the data
+    X, Y = np.meshgrid(np.arange(20, 51, 0.5), np.arange(0, 3.1, 0.5))
+    XX = X.flatten()
+    YY = Y.flatten()
+
+    order = 2    # 1: linear, 2: quadratic
+    if order == 1:
+        # best-fit linear plane
+        A = np.c_[data[:, 0], data[:, 1], np.ones(data.shape[0])]
+        C, _, _, _ = scipy.linalg.lstsq(A, data[:, 2])    # coefficients
+
+        # evaluate it on grid
+        Z = C[0]*X + C[1]*Y + C[2]
+
+        # or expressed using matrix/vector product
+        # Z = np.dot(np.c_[XX, YY, np.ones(XX.shape)], C).reshape(X.shape)
+
+    elif order == 2:
+        # best-fit quadratic curve
+        A = np.c_[np.ones(data.shape[0]), data[:, :2],
+                  np.prod(data[:, :2], axis=1), data[:, :2]**2]
+        C, _, _, _ = scipy.linalg.lstsq(A, data[:, 2])
+
+        # evaluate it on a grid
+        Z = np.dot(np.c_[np.ones(XX.shape), XX, YY, XX*YY,
+                         XX**2, YY**2], C).reshape(X.shape)
+    return X, Y, Z
 
 
 if __name__ == '__main__':
@@ -22,13 +55,13 @@ if __name__ == '__main__':
     # Can be one of: ['prec', 'rec', 'F1', 'acc', 'MR']
     performance_measure = 'MR'
 
-    x_measure = 'knn_k'
+    x_measure = 'm'
     y_measure = 'noise_spread'
     # Only use this if you want the z-measure to be another parameter
     # z_measure = 'm'
     # To include all data use alpha 0 as omitted set
-    omitted_measure = 'alpha'
-    omitted_target = 0
+    omitted_measure = 'knn_k'
+    omitted_target = 9
 
     # Aggregate the performance results together:
     # Each datapoint has a list of results (k results from k-fold cross-val)
@@ -54,9 +87,12 @@ if __name__ == '__main__':
     # Take the mean of each list of results to plot on the z-axis
     p = [sum(results) / len(results) for results in res_list]
 
+    X, Y, Z = fit_plane(x, y, p, 2)
+
     # Plot the results
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.8)
 
     cmap = mpl.cm.get_cmap('jet_r')
     plot = ax.scatter(x, y, p, c=p, cmap=cmap)
