81f49aef3ce1060d36ad0c18f82fefe49bad1d68
Travis Hammond <dashdeckers@gmail.com>
2020-02-11 14:28:32 +0100

    Data generator attempt

--- Deep Learning/CNN.py
+++ Deep Learning/CNN.py
@@ -1,73 +1,69 @@
 # import numpy as np
 import os
-import pickle
 
+import numpy as np
 import tensorflow as tf
 from experiments import minimal_model, small_model, standard_experiment  # noqa
 from keras.datasets import cifar10
 from keras.models import Sequential
-from keras.utils import to_categorical
-from keras.backend import resize_images
+from keras.utils import Sequence, to_categorical
+from skimage.transform import resize
 
 # Turn off Tensorflow Warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
-def load_cifar(enlarged=True):
-    if enlarged:
-        try:
-            file = open("cifar224.pkl", 'rb')
-            (x_train, y_train), (x_test, y_test) = pickle.load(file)
-            file.close()
-        except Exception:
-            (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-            # One hot encode
-            y_test = to_categorical(y_test, 10)
-            y_train = to_categorical(y_train, 10)
+class CIFAR10(Sequence):
+    def __init__(self, X, y, batch_size, preprocess=True, rescale=True):
+        self.X, self.y = X, y
+        self.rescale = rescale
+        self.batch_size = batch_size
 
-            # Normalize
-            x_train = (x_train.astype('float16') / 255)
-            x_test = (x_test.astype('float16') / 255)
+        if preprocess:
+            self.X = (X.astype('float32') / 255)
+            self.y = to_categorical(y, 10)
 
-            # Scale 32x32 to 244x244
-            x_train = resize_images(x_train, height_factor=7, width_factor=7,
-                                    data_format="channels_last").numpy()
-            x_test = resize_images(x_test, height_factor=7, width_factor=7,
-                                   data_format="channels_last").numpy()
+    def __len__(self):
+        return int(np.ceil(len(self.X) / float(self.batch_size)))
 
-            file = open("cifar224.pkl", 'wb')
-            pickle.dump(((x_train, y_train), (x_test, y_test)), file)
-            file.close()
-    else:
-        # Load data, one-hot-encode labels
-        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-        y_test = to_categorical(y_test, 10)
-        y_train = to_categorical(y_train, 10)
+    def __getitem__(self, idx):
+        batch_x = self.X[idx * self.batch_size: (idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size: (idx + 1) * self.batch_size]
 
-        # Normalize
-        x_train = (x_train.astype('float32') / 255)
-        x_test = (x_test.astype('float32') / 255)
-
-    return (x_train, y_train), (x_test, y_test)
+        return (
+            np.array([resize(img, (224, 224)) for img in batch_x]),
+            np.array(batch_y)
+        )
 
 
-# May want to move this elsewhere
-(x_train, y_train), (x_test, y_test) = load_cifar(enlarged=True)
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 
-def run_experiment(experiment, architecture, verbose=1):
-    model = Sequential(architecture)
+def run_experiment(experiment, model, verbose=1):
+    model = Sequential(model)
     model.compile(loss=experiment['loss'],
                   optimizer=experiment['optimizer'],
                   metrics=experiment['metrics'])
     model.build(x_train.shape[1:])
     model.summary()
-    model.fit(x_train, y_train,
-              batch_size=experiment['batch_size'],
-              epochs=experiment['epochs'],
-              verbose=verbose)
-    print(model.evaluate(x_test, y_test, verbose=verbose))
+
+    batch_size = experiment['batch_size']
+
+    train_generator = CIFAR10(x_train, y_train, batch_size=batch_size)
+    test_generator = CIFAR10(x_test, y_test, batch_size=batch_size)
+
+    model.fit_generator(train_generator,
+                        epochs=experiment['epochs'],
+                        verbose=verbose,
+                        validation_data=test_generator,
+                        workers=6,
+                        use_multiprocessing=True)
+
+    print(model.evaluate_generator(test_generator,
+                                   verbose=verbose,
+                                   workers=6,
+                                   use_multiprocessing=True))
     return model
 
 
