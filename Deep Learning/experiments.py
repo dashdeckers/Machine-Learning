e6e8cf28bcda95d493d7fae9eda3613fa2273097
Travis Hammond <dashdeckers@gmail.com>
2020-02-11 12:23:18 +0100

    AlexNet Model

--- Deep Learning/experiments.py
+++ Deep Learning/experiments.py
@@ -1,4 +1,9 @@
-from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D  # noqa
+import numpy as np
+from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
+
+# Seed the rng's for reproducible results
+seed = 1000
+np.random.seed(seed)
 
 # Define experiments
 standard_experiment = {
@@ -11,6 +16,82 @@ standard_experiment = {
 
 
 # Define models
+AlexNet = [
+    # 1st Conv Layer
+    Conv2D(filters=96,
+           input_shape=(224, 224, 3),
+           kernel_size=(11, 11),
+           strides=(4, 4),
+           padding='valid',
+           activation='relu'),
+
+    MaxPooling2D(pool_size=(3, 3),
+                 strides=(2, 2),
+                 padding='valid'),
+
+    # 2nd Conv Layer
+    Conv2D(filters=256,
+           kernel_size=(5, 5),
+           strides=(1, 1),
+           padding='valid',
+           activation='relu'),
+
+    MaxPooling2D(pool_size=(3, 3),
+                 strides=(2, 2),
+                 padding='valid'),
+
+    # 3rd Conv Layer
+    Conv2D(filters=384,
+           kernel_size=(3, 3),
+           strides=(1, 1),
+           padding='valid',
+           activation='relu'),
+
+    # 4th Conv Layer
+    Conv2D(filters=384,
+           kernel_size=(3, 3),
+           strides=(1, 1),
+           padding='valid',
+           activation='relu'),
+
+    # 5th Conv Layer
+    Conv2D(filters=256,
+           kernel_size=(3, 3),
+           strides=(1, 1),
+           padding='valid',
+           activation='relu'),
+
+    MaxPooling2D(pool_size=(3, 3),
+                 strides=(2, 2),
+                 padding='valid'),
+
+    # Flatten
+    Flatten(),
+
+    # 1st Fully Connected Layer
+    Dense(units=4096,
+          input_shape=(224*224*3,),
+          activation='relu'),
+    Dropout(rate=0.4,
+            seed=seed+1),
+
+    # 2nd Fully Connected Layer
+    Dense(units=4096,
+          activation='relu'),
+    Dropout(rate=0.4,
+            seed=seed+2),
+
+    # 3rd Fully Connected Layer
+    Dense(units=4096,
+          activation='relu'),
+    Dropout(rate=0.4,
+            seed=seed+3),
+
+    # Output Layer
+    Dense(units=10,
+          activation='softmax')
+]
+
 small_model = [
     Conv2D(filters=96,
            input_shape=(32, 32, 3),
