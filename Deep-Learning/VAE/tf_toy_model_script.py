8a4c602eee2fd52797ac603c51b5a3950961553c
Travis Hammond <dashdeckers@gmail.com>
2020-04-24 16:48:50 +0200

    Script can now handle stanford_dogs

--- Deep-Learning/VAE/tf_toy_model_script.py
+++ Deep-Learning/VAE/tf_toy_model_script.py
@@ -4,33 +4,25 @@ import math
 from os import makedirs, path
 
 import matplotlib.pyplot as plt
+import scipy
+import sklearn
 import tensorflow as tf
 from data import get_data
 from tensorflow.keras import layers
 
-exp = {
-    'dataset': 'mnist',
-    'im_shape': (28, 28),
-    'channels': 1,
-    'latent_dim': 2,
-    'batch_size': 64,
-    'epochs': 10,
-    'beta': 1.0,
-    'model_path': 'mnist_2',
-    'checkpoint_format': '{epoch:02d}-{val_loss:.2f}.h5',
-}
-
 
 def show_shapes(input_shape, layer_list, name):
-    # Basic visualization of tensor shapes to track architecture and flow.
-    # Layers must be successive starting from input_shape or it won't work.
-    print('')
+    """Show the tensor shapes that flow through the model.
 
+    Basic visualization of tensor shapes to track architecture and flow.
+    Layers must be successive starting from input_shape or it won't work,
+    and the Sampling layer does not work either, use dense_mean instead.
+    """
     shapes = [input_shape]
     for layer in layer_list:
         shapes.append(layer.compute_output_shape(shapes[-1]))
 
-    print(f'{name}:', ' --> '.join([str(s[1:]) for s in shapes]), '\n')
+    print(f'\n{name}:', ' --> '.join([str(s[1:]) for s in shapes]), '\n')
 
 
 class Sampling(layers.Layer):
@@ -57,36 +49,21 @@ class Sampling(layers.Layer):
 class Encoder(tf.keras.Model):
     """Maps an image to: (z_mean, z_log_var, z)."""
 
-    def __init__(self, latent_dim, name='encoder', **kwargs):
+    def __init__(self, exp, name='encoder', **kwargs):
         super(Encoder, self).__init__(name=name, **kwargs)
-        self.latent_dim = latent_dim
+        self.latent_dim = exp['latent_dim']
 
     def build(self, input_shape):
-        # First two convolutional layers to capture image data
+        # First layers to capture image data
         self.layer_list = [
-            layers.Conv2D(
-                input_shape=input_shape,
-                data_format='channels_last',
-                filters=32,
-                kernel_size=2,
-                padding='same',
-                activation='relu',
-                name='Conv-1',
-            ),
-            layers.Conv2D(
-                filters=64,
-                kernel_size=2,
-                padding='same',
-                activation='relu',
-                strides=(2, 2),
-                name='Conv-2',
-            ),
             layers.Flatten(),
             layers.Dense(256, activation='relu'),
             layers.Dense(128, activation='relu'),
         ]
+        # Then split with two dense outputs for (z_mean, z_log_var)
         self.dense_mean = layers.Dense(units=self.latent_dim)
         self.dense_log_var = layers.Dense(units=self.latent_dim)
+        # Sample (z) from (z_mean, z_log_var)
         self.sampling = Sampling(latent_dim=self.latent_dim)
 
         show_shapes(
@@ -100,24 +77,21 @@ class Encoder(tf.keras.Model):
         for layer in self.layer_list:
             output = layer(output)
 
-        # Split with two dense outputs for (z_mean, z_log_var)
         z_mean = self.dense_mean(output)
         z_log_var = self.dense_log_var(output)
 
-        # Sample (z) from (z_mean, z_log_var)
         z = self.sampling((z_mean, z_log_var))
-
         return z_mean, z_log_var, z
 
 
 class Decoder(tf.keras.Model):
     """Converts z back into an image."""
 
-    def __init__(self, latent_dim, im_dim, name='decoder', **kwargs):
+    def __init__(self, exp, name='decoder', **kwargs):
         super(Decoder, self).__init__(name=name, **kwargs)
-        self.latent_dim = latent_dim
-        self.col_dim = im_dim[0] * im_dim[1] * im_dim[2]
-        self.im_dim = im_dim
+        self.latent_dim = exp['latent_dim']
+        self.im_dim = exp['im_shape'] + (exp['channels'], )
+        self.col_dim = self.im_dim[0] * self.im_dim[1] * self.im_dim[2]
 
     def build(self, input_shape):
         assert input_shape[1:] == (self.latent_dim), input_shape[1:]
@@ -145,11 +119,33 @@ class VariationalAutoEncoder(tf.keras.Model):
         self.im_dim = exp['im_shape'] + (exp['channels'], )
         self.beta = exp['beta']
         self.model_path = exp['model_path']
+        self.exp = exp
 
     def build(self, input_shape):
         assert len(input_shape) == 4, f'(batch, w, h, c): {input_shape}'
-        self.encoder = Encoder(latent_dim=self.latent_dim)
-        self.decoder = Decoder(latent_dim=self.latent_dim, im_dim=self.im_dim)
+        self.encoder = Encoder(exp=self.exp)
+        self.decoder = Decoder(exp=self.exp)
+
+    """
+    Just testing these methods.
+    """
+    @staticmethod
+    def kl_divergence(p, q):
+        assert len(p) == len(q), 'Must have the same length'
+        assert sum(p) == sum(q) == 1, 'Must sum to 1 to be a probability dist.'
+        return sum(p[i] * math.log2(p[i]/q[i]) for i in range(len(p)))
+
+    @staticmethod
+    def kl_divergence_lib(p, q):
+        return (scipy.rel_entr(p, q), scipy.kl_div(p, q))
+
+    @staticmethod
+    def mutual_information(p, q):
+        return sklearn.metrics.mutual_info_score(p, q)
+
+    """
+    Don't rely on them just yet.
+    """
 
     def call(self, inputs):
         z_mean, z_log_var, z = self.encoder(inputs)
@@ -178,17 +174,20 @@ class VariationalAutoEncoder(tf.keras.Model):
 
         self.add_loss(tf.reduce_mean(rec_loss + kl_loss), inputs=inputs)
 
-        # TODO: Save reconstructed image everytime to see the quality increase?
+        # breakpoint()
 
+        # TODO: Save reconstructed image everytime to see the quality increase?
         return reconstructed
 
 
-def get_model(model_path, input_shape):
+def get_model(exp):
     """Load a model from checkpoint or create a new one.
 
     Manages loading from most recent checkpoint, creating the
     directories, returning the model.
     """
+    model_path = exp['model_path']
+    input_shape = (1,) + exp['im_shape'] + (exp['channels'], )
 
     vae = VariationalAutoEncoder(exp)
     vae.compile(optimizer=tf.keras.optimizers.Adam())
@@ -229,6 +228,32 @@ def show_reconstructed_digit(vae, index=0):
     plt.show()
 
 
+# Define the experiment
+# exp = {
+#     'dataset': 'mnist',
+#     'im_shape': (28, 28),
+#     'channels': 1,
+#     'latent_dim': 2,
+#     'batch_size': 64,
+#     'epochs': 10,
+#     'beta': 1.0,
+#     'model_path': 'new_mnist',
+#     'checkpoint_format': '{epoch:02d}-{val_loss:.2f}.h5',
+# }
+
+exp = {
+    'dataset': 'stanford_dogs',  # 'mnist',
+    'im_shape': (32, 32),  # (28, 28),
+    'channels': 3,
+    'latent_dim': 64,
+    'batch_size': 16,
+    'epochs': 10,
+    'beta': 1.0,
+    'model_path': 'new_dogs',
+    'checkpoint_format': '{epoch:02d}-{val_loss:.2f}.h5',
+}
+
+
 # Load and preprocess the data
 train, test, info = get_data(
     batch_size=exp['batch_size'],
@@ -238,7 +263,7 @@ train, test, info = get_data(
 
 
 # Create, train and evaluate the model
-vae = get_model(exp['model_path'], input_shape=(1, 28, 28, 1))
+vae = get_model(exp)
 vae.fit(
     train,
     epochs=exp['epochs'],
@@ -253,14 +278,15 @@ vae.fit(
             filename=path.join(exp['model_path'], 'losses.csv'),
             append=True
         ),
-        # Create model checkpoints
-        tf.keras.callbacks.ModelCheckpoint(
-            filepath=path.join(exp['model_path'], exp['checkpoint_format']),
-            monitor='val_loss',
-            save_freq='epoch',
-            verbose=1,
-        ),
+        # Create model checkpoints (Skip during exploratory testing)
+        # tf.keras.callbacks.ModelCheckpoint(
+        #     filepath=path.join(exp['model_path'], exp['checkpoint_format']),
+        #     monitor='val_loss',
+        #     save_freq='epoch',
+        #     verbose=1,
+        # ),
     ],
 )
+
 
 show_reconstructed_digit(vae)
