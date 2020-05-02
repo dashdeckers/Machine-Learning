29a2fb317800d65ce4f9b79692415c337a882b04
René Flohil <rflowhill@gmail.com>
2020-05-02 21:14:18 +0200

    Update dog_architecture_layers.py

--- Deep-Learning/VAE/dog_architecture_layers.py
+++ Deep-Learning/VAE/dog_architecture_layers.py
@@ -78,3 +78,94 @@ exp['decoder_layers'] = [
         ),
         layers.Reshape(target_shape=exp['input_shape'][1:]),
     ]
+
+# Architecture 3 - Bigger Images
+# Define the experiment
+    exp = {
+        'project_name': project_name,
+        'dataset': 'stanford_dogs',  # 'mnist'
+        'input_shape': (1, 64, 64, 3),  # (1, 28, 28, 1)
+        'batch_size': 64,
+        'epochs': 100,
+
+        'latent_dim': 20,
+        'alpha': 1.0,
+        'beta': 1.0,
+        'gamma': 1.0,
+        'distribution': 'gaussian',
+
+        'optimizer': tf.keras.optimizers.Adam(),
+    }
+    exp['im_shape'] = exp['input_shape'][1:3]
+    exp['channels'] = exp['input_shape'][3]
+    exp['col_dim'] = int(np.prod(exp['input_shape'][1:]))
+
+    # Define the architecture
+    exp['encoder_layers'] = [
+        layers.Conv2D(
+            filters=exp['input_shape'][1],
+            kernel_size=(3, 3),
+            strides=(2, 2),
+            padding='same',
+            input_shape=exp['input_shape'],
+            activation='relu',
+            data_format='channels_last'
+        ),
+        layers.Conv2D(
+            filters=exp['input_shape'][1] * 2,
+            kernel_size=(3, 3),
+            strides=(2, 2),
+            padding='same',
+            activation='relu',
+            data_format='channels_last'
+        ),
+        layers.Conv2D(
+            filters=exp['input_shape'][1] * 4,
+            kernel_size=(3, 3),
+            strides=(2, 2),
+            padding='same',
+            activation='relu',
+            data_format='channels_last'
+        ),
+        layers.Flatten(),
+        layers.Dense(
+            units=exp['input_shape'][1] * 4,
+            activation='relu'
+        ),
+    ]
+
+    exp['decoder_layers'] = [
+        layers.Dense(
+            units=exp['input_shape'][1] * 4,
+            activation='relu'
+        ),
+        layers.Dense(
+            units=16384,
+            activation='relu'
+        ),
+        layers.Reshape(target_shape=(8, 8, 256)),
+        layers.Conv2DTranspose(
+            filters=exp['input_shape'][1] * 2,
+            kernel_size=(4, 4),
+            strides=(2, 2),
+            padding='same',
+            activation='relu',
+            data_format='channels_last'
+        ),
+        layers.Conv2DTranspose(
+            filters=exp['input_shape'][1],
+            kernel_size=(4, 4),
+            strides=(2, 2),
+            padding='same',
+            activation='relu',
+            data_format='channels_last'
+        ),
+        layers.Conv2DTranspose(
+            filters=exp['channels'],
+            kernel_size=(4, 4),
+            strides=(2, 2),
+            padding='same',
+            activation='relu',
+            data_format='channels_last'
+        ),
+    ]
