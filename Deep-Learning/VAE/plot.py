be9475453af9526d7ce02a68a4225ad4123bb324
René Flohil <rflowhill@gmail.com>
2020-05-03 14:20:27 +0200

    Upload Arch4

--- Deep-Learning/VAE/plot.py
+++ Deep-Learning/VAE/plot.py
@@ -256,5 +256,5 @@ if __name__ == '__main__':
     # Use CTRL+C to quit early
     plot_losses(args.name, ['decomp', 'main'])
     plot_digit_classes_in_latent_space(vae.encoder, exp)
-    # plot_independent_grid(vae.decoder, exp)
-    plot_all_2D_manifolds(vae.decoder, exp)
+    plot_independent_grid(vae.decoder, exp)
+    # plot_all_2D_manifolds(vae.decoder, exp)
