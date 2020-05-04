7e94c3eb52e62352f1794ba3ee616e3c098e81c8
René Flohil <rflowhill@gmail.com>
2020-05-04 10:17:35 +0200

    More epochs

--- Deep-Learning/VAE/experiment.py
+++ Deep-Learning/VAE/experiment.py
@@ -49,7 +49,7 @@ def get_experiment(project_name, resume=False):
         'dataset': 'stanford_dogs',  # 'mnist'
         'input_shape': (1, 64, 64, 3),  # (1, 28, 28, 1)
         'batch_size': 64,
-        'epochs': 25,
+        'epochs': 50,
 
         'latent_dim': 120,
         'alpha': 1.0,
