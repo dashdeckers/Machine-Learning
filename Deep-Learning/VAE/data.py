c29aae7b1e212c40ea8d8625236cbd50c69712b5
hiddefolkertsma <h.folkertsma.1@student.rug.nl>
2020-05-08 20:19:25 +0200

    add some instructory comments, uncomment dog cropping

--- Deep-Learning/VAE/data.py
+++ Deep-Learning/VAE/data.py
@@ -71,18 +71,25 @@ def get_data(batch_size, im_shape, labels=False, dataset='stanford_dogs'):
     preprocess = partial(preprocessing, im_shape=im_shape, labels=labels)
 
     if (dataset == "celeb_a"):
+        # celeba is in TFDS, but frequently this throws errors due to it being hosted on Google Drive
+        # it can be downloaded from https://www.kaggle.com/jessicali9530/celeba-dataset/data
+        # change the path to your local copy of celeba
         celeb_a = tf.data.Dataset.list_files(str('/data/s2759799/img_align_celeba/img_align_celeba/*'))
         celeb_a = celeb_a.map(read_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        test_data = celeb_a.take(8000)
-        train_data = celeb_a.skip(8000).take(40000)
+        test_data = celeb_a.take(8000) # use 8000 test samples
+        train_data = celeb_a.skip(8000).take(40000) # use 40000 training samples
         steps_per_epoch = int(tf.math.ceil(40000 / batch_size))
         validation_steps = int(tf.math.ceil(8000 / batch_size))
 
     elif (dataset == "chairs"):
+        # the chairs dataset is not in TFDS
+        # it can be downloaded from https://www.di.ens.fr/willow/research/seeing3Dchairs/
+        # change the path to your local copy of the chairs dataset
+        # make sure to move all the images to the root folder
         chairs = tf.data.Dataset.list_files(str('/data/s2759799/rendered_chairs/*.png*'))
         chairs = chairs.map(read_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        test_data = chairs.take(8000)
-        train_data = chairs.skip(8000).take(40000)
+        test_data = chairs.take(8000) # use 8000 test samples
+        train_data = chairs.skip(8000).take(40000) # use 40000 training samples
         steps_per_epoch = int(tf.math.ceil(40000 / batch_size))
         validation_steps = int(tf.math.ceil(8000 / batch_size))
 
@@ -105,9 +112,9 @@ def get_data(batch_size, im_shape, labels=False, dataset='stanford_dogs'):
                 info.splits["test"].num_examples / batch_size
         ))
 
-    # if dataset == 'stanford_dogs':
-        # crop_dogs(train_data)
-        # crop_dogs(test_data)
+    if dataset == 'stanford_dogs':
+        crop_dogs(train_data)
+        crop_dogs(test_data)
 
     train = (
         train_data
