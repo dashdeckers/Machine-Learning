88aefdd3d55875ebeb3f820efc9a55db5102ffcf
Travis Hammond <dashdeckers@gmail.com>
2020-06-12 18:04:08 +0200

    Update to plot.py

--- plot.py
+++ plot.py
@@ -3,7 +3,6 @@ import json
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.interpolate import splev, splrep
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
 # Define strings for TB logging lines
@@ -15,11 +14,11 @@ joins = ["Conjoined", "Disjoined"]
 append = "-tag-dat.json"
 
 # Concatenate combinations of these strings
-
 cores = list(itertools.product(metrics, datas, joins))
 core_strings = ['_'.join(line)for line in cores]
 path_strings = [runs + core_string + append for core_string in core_strings]
 
+# Load the data from the strings
 data_dict = dict()
 for path in path_strings:
     try:
@@ -29,135 +28,108 @@ for path in path_strings:
     except FileNotFoundError:
         print("No file " + 'json/'+path)
 
-### TRAIN/VAL Con
-loss_val = "run-loss_val_Conjoined-tag-dat.json"
-loss_train = "run-loss_train_Conjoined-tag-dat.json"
 
-plt.scatter(data_dict[loss_train][:, 1], data_dict[loss_train][:, 2], label="Train", alpha=0.2)
-plt.scatter(data_dict[loss_val][:, 1], data_dict[loss_val][:, 2], label="Validation", alpha=0.2)
+# TRAIN/VAL Losses
 
-# poly = splrep(data_dict[loss_train][:, 1], data_dict[loss_train][:, 2], s=1)
-# poly_y = splev(data_dict[loss_train][:, 1], poly)
-ys = lowess(data_dict[loss_train][:, 2], data_dict[loss_train][:, 1], frac=0.5)
 
-plt.plot(data_dict[loss_train][:, 1], ys, c='C0')
+fig, axs = plt.subplots(nrows=1, ncols=2)
 
-ys = lowess(data_dict[loss_val][:, 2], data_dict[loss_val][:, 1], frac=0.5)
+mtl_loss_val = "run-loss_val_Conjoined-tag-dat.json"
+mtl_loss_train = "run-loss_train_Conjoined-tag-dat.json"
 
-plt.plot(data_dict[loss_val][:, 1], ys, c='C1')
+stl_loss_val = "run-loss_val_Disjoined-tag-dat.json"
+stl_loss_train = "run-loss_train_Disjoined-tag-dat.json"
 
-plt.legend()
-plt.ylim(0.05, 0.065)
-plt.xlabel("Batch")
-plt.ylabel("BCE loss")
+losses = [
+    (mtl_loss_train, mtl_loss_val, 'MTL'),
+    (stl_loss_train, stl_loss_val, 'STL')
+]
 
-plt.title("Train and validation loss for Conjoined network")
+for plt_idx, (train_key, val_key, model) in enumerate(losses):
+    train = data_dict[train_key]
+    val = data_dict[val_key]
 
+    for c_idx, (dataset, label) in enumerate([(train, 'train'), (val, 'val')]):
+        color = 'C' + str(c_idx)
+        label = model + '_' + label
+
+        # The datapoints
+        axs[plt_idx].scatter(
+            dataset[:, 1], dataset[:, 2],
+            label=label, alpha=0.2, c=color
+        )
+
+        # The line
+        smoothed = lowess(dataset[:, 2], dataset[:, 1], frac=0.5)
+        axs[plt_idx].plot(dataset[:, 1], smoothed, c=color)
+
+        # The error lines + shading
+        abs_diff = abs(dataset[:, 2] - smoothed[:, 1])
+        smoothed_diff = lowess(abs_diff, dataset[:, 1], frac=0.5)
+
+        axs[plt_idx].fill_between(
+            dataset[:, 1],
+            (smoothed - smoothed_diff)[:, 1],
+            (smoothed + smoothed_diff)[:, 1],
+            alpha=0.5,
+            edgecolor=color,
+            facecolor=color
+        )
+
+    # The rest
+    axs[plt_idx].legend()
+    axs[plt_idx].set_ylim(0.02, 0.15)  # 0.05, 0.065 || 0.045, 0.09
+    axs[plt_idx].set_xlabel("Batch")
+
+axs[0].set_ylabel("BCE loss")
+axs[0].set_title("MTL model")
+axs[1].set_title("STL model")
+
+axs[1].tick_params(
+    axis='y',
+    which='both',
+    left=False,
+    labelleft=False
+)
+
+assert axs[0].get_ylim() == axs[1].get_ylim(), axs[0].get_ylim()
+
+fig.suptitle('Train and validation loss for the MTL and STL models')
+fig.tight_layout(rect=[0, 0.03, 1, 0.95])  # Make the title show right
 plt.show()
 
 
-### TRAIN/VAL Dis
-loss_val = "run-loss_val_Disjoined-tag-dat.json"
-loss_train = "run-loss_train_Disjoined-tag-dat.json"
+# TRAIN/VAL Accuracies
 
-plt.scatter(data_dict[loss_train][:, 1], data_dict[loss_train][:, 2], label="Train", alpha=0.2)
-plt.scatter(data_dict[loss_val][:, 1], data_dict[loss_val][:, 2], label="Validation", alpha=0.2)
 
-ys = lowess(data_dict[loss_train][:, 2], data_dict[loss_train][:, 1], frac=0.5)
+pred_mtl = "run-Pred_corrects_val_Conjoined-tag-dat.json"
+pred_stl = "run-Pred_corrects_val_Disjoined-tag-dat.json"
 
-plt.plot(data_dict[loss_train][:, 1], ys,  c='C0')
+clas_mtl = "run-Class_corrects_val_Conjoined-tag-dat.json"
+clas_stl = "run-Class_corrects_val_Disjoined-tag-dat.json"
 
-ys = lowess(data_dict[loss_val][:, 2], data_dict[loss_val][:, 1], frac=0.5)
+comb_mtl = "run-True_corrects_val_Conjoined-tag-dat.json"
+comb_stl = "run-True_corrects_val_Disjoined-tag-dat.json"
 
-plt.plot(data_dict[loss_val][:, 1], ys, c='C1')
+data_labels = [
+    (pred_mtl, 'MTL prediction', 'darkblue'),
+    (pred_stl, 'STL prediction', 'lightblue'),
 
-plt.legend()
-plt.ylim(0.05, 0.065)
-plt.xlabel("Batch")
-plt.ylabel("BCE loss")
+    (clas_mtl, 'MTL classification', 'darkorange'),
+    (clas_stl, 'STL classification', 'gold'),
 
-plt.title("Train and validation loss for Disjoined network")
+    (comb_mtl, 'MTL combined', 'darkviolet'),
+    (comb_stl, 'STL combined', 'fuchsia'),
+]
 
-plt.show()
-
-### LOSS
-
-loss_con = "run-loss_val_Conjoined-tag-dat.json"
-loss_dis = "run-loss_val_Disjoined-tag-dat.json"
-plt.plot(data_dict[loss_con][:, 1], data_dict[loss_con][:, 2], label="Conjoined")
-plt.plot(data_dict[loss_dis][:, 1], data_dict[loss_dis][:, 2], label="Disjoined")
-
-# poly = splrep(data_dict[loss_con][:, 1], data_dict[loss_con][:, 2], s=100000)
-# poly_y = splev(data_dict[loss_con][:, 1], poly)
-
-# # plt.plot(data_dict[loss_con][:, 1], poly_y, label="Conjoined")
-
-# poly = splrep(data_dict[loss_dis][:, 1], data_dict[loss_dis][:, 2], s=100000)
-# poly_y = splev(data_dict[loss_dis][:, 1], poly)
-
-# plt.plot(data_dict[loss_dis][:, 1], poly_y, label="Disjoined")
+for key, label, color in data_labels:
+    plt.plot(data_dict[key][:, 1], data_dict[key][:, 2],
+             label=label, alpha=1, c=color)
 
 plt.legend()
-plt.ylim(0.05, 0.065)
+plt.ylim(0.5, 0.875)
 plt.xlabel("Batch")
-plt.ylabel("BCE loss")
-
-plt.title("Validation loss")
-
-plt.show()
-
-
-### PREDICTION CORRECT
-
-metric_con = "run-Pred_corrects_val_Conjoined-tag-dat.json"
-metric_dis = "run-Pred_corrects_val_Disjoined-tag-dat.json"
-plt.plot(data_dict[metric_con][:, 1], data_dict[metric_con][:, 2],
-         label="Conjoined", alpha=1)
-plt.plot(data_dict[metric_dis][:, 1], data_dict[metric_dis][:, 2],
-         label="Disjoined", alpha=1)
-
-plt.legend()
-plt.ylim(0.76, 0.8)
-plt.xlabel("Batch")
-plt.ylabel("Prediction accuracy")
-
-plt.title("Validation prediction accuracy")
-
-plt.show()
-
-
-### CLASS CORRECT
-
-metric_con = "run-Class_corrects_val_Conjoined-tag-dat.json"
-metric_dis = "run-Class_corrects_val_Disjoined-tag-dat.json"
-plt.plot(data_dict[metric_con][:, 1], data_dict[metric_con][:, 2],
-         label="Conjoined", alpha=1)
-plt.plot(data_dict[metric_dis][:, 1], data_dict[metric_dis][:, 2],
-         label="Disjoined", alpha=1)
-
-plt.legend()
-plt.ylim(0.83, 0.88)
-plt.xlabel("Batch")
-plt.ylabel("Recognition accuracy")
-
-plt.title("Validation recognition accuracy")
-
-plt.show()
-
-
-### CLASS CORRECT
-
-metric_con = "run-True_corrects_val_Conjoined-tag-dat.json"
-metric_dis = "run-True_corrects_val_Disjoined-tag-dat.json"
-plt.plot(data_dict[metric_con][:, 1], data_dict[metric_con][:, 2],
-         label="Conjoined", alpha=1)
-plt.plot(data_dict[metric_dis][:, 1], data_dict[metric_dis][:, 2],
-         label="Disjoined", alpha=1)
-plt.legend()
-plt.ylim(0.76, 0.8)
-plt.xlabel("Batch")
-plt.ylabel("Resulting accuracy")
-
-plt.title("Validation accuracy of final output")
+plt.ylabel("Accuracy")
+plt.title("Validation accuracies")
 
 plt.show()
